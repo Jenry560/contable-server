a46eb4ea4ab91c8b1307d9052f1fda25716d2983
Jenry Sanchez <henryfuerte560@gmail.com>
2024-03-05 08:49:23 -0400

    nueva lineas

--- db/mongo.js
+++ db/mongo.js
@@ -1,10 +1,16 @@
-const mongoose = require('mongoose')
-require("dotenv").config()
-mongoose.connect("mongodb+srv://Jenry:Jenry560@cluster0.tkoex7w.mongodb.net/contable?retryWrites=true&w=majority")
+const mongoose = require("mongoose");
+require("dotenv").config();
+mongoose.connect(
+  "mongodb+srv://Jenry:Jenry560@cluster0.tkoex7w.mongodb.net/contable?retryWrites=true&w=majority"
+);
 
-const objetodb = mongoose.connection
+const objetodb = mongoose.connection;
 
-objetodb.on("connected",()=>{console.log("Base de dato conectada")})
-objetodb.on("error",()=>{console.log("error")})
+objetodb.on("connected", () => {
+  console.log("Base de dato conectada");
+});
+objetodb.on("error", () => {
+  console.log("error");
+});
 
-module.exports = mongoose
+module.exports = mongoose;
